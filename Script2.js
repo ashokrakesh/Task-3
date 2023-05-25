7996823c490701e84063afc4abc3592ffe62b107
ashokrakesh <126311525+ashokrakesh@users.noreply.github.com>
2023-05-25 10:27:53 +0530

    Json Resume

---
+++ Script2.js
@@ -0,0 +1,59 @@
+let myResume={
+    "basics": {
+      "name": "RAKESH A",
+      "email": "rakeshashok93@gamil.com",
+      "phone": 9944664330,
+      "degree": "B.E",
+      "location": {
+        "address": "D1-106 Akshaya Today Chennai",
+        "postalCode": 603103,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Sutherland Global Service",
+        "position": "Associate",
+        "startDate": "2019-10-07",
+        "endDate": "2023-05-24",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "RVS College Of Engineering",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2011,
+        "batch end year": 2015,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginner",
+        "project": [
+          "automatic attendance maintaing system using Java language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,Cricketer,",
+      }
+    ]
+  }
+  console.log(myResume);
